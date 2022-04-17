65a61e10632c94df7abbf06077f894565dc71d10
Ryoichi Izumita <r.izumita@caph.jp>
2022-04-17 22:39:42 +0900

    Implement basic feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as notifier from 'node-notifier'
+import * as vscode from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.tasks.onDidEndTask((event) => { 
+		const style = vscode.workspace.getConfiguration('taskNotification').get('style')
+		const message = createMessage(event)
+
+		switch (style) {
+			case 'system':
+				const playSound = vscode.workspace.getConfiguration('taskNotification').get('playSound') as boolean
+				notifier.notify({ title: 'Visual Studio Code', message: message, sound: playSound })
+				break
+			case 'vscode':
+				vscode.window.showInformationMessage(message)
+				break
+		}
+	})
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-task-notification" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-task-notification.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TaskNotification!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function createMessage(event: vscode.TaskEndEvent): string {
+	const taskName = event.execution.task.name
+	return `${taskName} finished`
+}
